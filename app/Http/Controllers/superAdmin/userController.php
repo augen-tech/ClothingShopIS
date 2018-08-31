a6c4c3ff9e6a3ac3c84c9e217bf833ded705e812
januarelsan <januarelsan@gmail.com>
2018-09-01 00:21:09 +0700

    m

--- app/Http/Controllers/superAdmin/userController.php
+++ app/Http/Controllers/superAdmin/userController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\superAdmin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class userController extends Controller
+class UserController extends Controller
 {
     public function listAdmin()
     {
